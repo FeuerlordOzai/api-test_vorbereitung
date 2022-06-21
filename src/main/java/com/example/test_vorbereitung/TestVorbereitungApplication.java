fca978ca5cd8c8b8da3bc287e25d7cd1af703931
Malek <malekbouazizi@gmail.com>
2022-06-21 21:08:43 +0200

    Initial commit

---
+++ src/main/java/com/example/test_vorbereitung/TestVorbereitungApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_vorbereitung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVorbereitungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVorbereitungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_vorbereitung/TestVorbereitungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_vorbereitung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVorbereitungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
